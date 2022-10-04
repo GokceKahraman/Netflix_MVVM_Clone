410b2e1dfbcb3e90aaf96a49cb1fd913e944f162
GokceKahraman <gokcekahraman0@gmail.com>
2022-10-04 15:22:15 +0300

    Initial Commit

---
+++ Netflix_MVVM_Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix_MVVM_Clone
+//
+//  Created by Gökçe Kahraman on 4.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
